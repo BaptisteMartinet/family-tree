151a4502292914f1081bde53ebf84ab9a85a2892
Baptiste Martinet <baptiste.martinet@epitech.eu>
2021-12-20 20:37:46 +0100

    deps: initialize graphql

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express();
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => { return 'hello world!'},
+};
+
+app.use('/graphql', graphqlHTTP({
+  graphiql: true,
+  schema: schema,
+  rootValue: root,
+}));
+
+app.listen(4000, () => console.log('Listening on port 4000...'));

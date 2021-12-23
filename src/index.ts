f683db6cb4807ccadda4ee31d842960aaab85e48
Baptiste Martinet <baptiste.martinet@epitech.eu>
2021-12-23 17:23:07 +0100

    deps: implement mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import express from 'express';
 import { GraphQLSchema } from 'graphql';
 import { graphqlHTTP } from 'express-graphql';
+import mongoose from 'mongoose';
 
 import queryType from './query';
 
-const app = express();
+const PORT = 3000;
 
-const schema = new GraphQLSchema({ query: queryType });
-
-app.use('/graphql', graphqlHTTP({
-  graphiql: true,
-  schema,
-}));
-
-app.listen(4000, () => console.log('Listening on port 4000...'));
+(async () => {
+  await mongoose.connect('mongodb://localhost:27017/family-tree');
+  console.info('Successfully connected to MongoDB.');
+  const schema = new GraphQLSchema({ query: queryType });
+  console.info('Successfully built the GraphQL schema.');
+  const app = express();
+  app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema,
+  }));
+  app.listen(4000, () => console.info(`GraphQL API listening on port ${PORT}...`));
+})();

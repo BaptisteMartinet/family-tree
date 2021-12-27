75c00a1112fafedd227a092649a64d0dc9c4c29d
Baptiste Martinet <baptiste.martinet@epitech.eu>
2021-12-28 00:49:19 +0100

    refactor(mutation): make Person mutation cleaner

--- src/mutation/index.ts
+++ src/mutation/index.ts
@@ -6,19 +6,34 @@ import { GraphQLDate } from '@utils/scalars';
 const mutationType = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    createPerson: {
-      type: PersonType,
-      description: 'Create a person in the family tree',
+    person: {
+      type: new GraphQLObjectType({
+        name: 'PersonMutation',
+        fields: {
+          create: {
+            type: PersonType,
+            args: {
+              firstName: { type: new GraphQLNonNull(GraphQLString) },
+              lastName: { type: new GraphQLNonNull(GraphQLString) },
+              birthDate: { type: GraphQLDate },
+              deathDate: { type: GraphQLDate },
+            },
+            async resolve(_, args, ctx) {
+              return await Person.create(args);
+            },
+          }
+        },
+      }),
       args: {
-        firstName: { type: new GraphQLNonNull(GraphQLString) },
-        lastName: { type: new GraphQLNonNull(GraphQLString) },
-        birthDate: { type: GraphQLDate },
-        deathDate: { type: GraphQLDate },
+        id: { type: GraphQLString },
       },
-      async resolve(_, args, ctx) {
-        return await Person.create(args);
-      },
-    },
+      async resolve(_, { id }, ctx) {
+        if (id === undefined)
+          return {};
+        const person = await Person.findById(id);
+        return person ?? {};
+      }
+    }
   },
 });
 

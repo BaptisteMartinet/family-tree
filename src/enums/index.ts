dcaa2e3aa144c3dc844bb36b1fe4b4a30f8577e5
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-01-07 19:39:07 +0100

    feat: rework enum to work with mongoose

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,14 +1,12 @@
 import { GraphQLEnumType } from 'graphql';
 
 export enum RelationTypeEnumT {
-  PARENT = 0,
-  CHILD,
+  PARENT = 'PARENT',
 }
 
 export const RelationTypeEnum = new GraphQLEnumType({
   name: 'RelationTypeEnum',
   values: {
-    PARENT: { value: 0 },
-    CHILD: { value: 1 },
+    PARENT: { value: 'PARENT' },
   },
 });
--- src/models/Relation.ts
+++ src/models/Relation.ts
@@ -1,9 +1,10 @@
 import mongoose, { Schema } from 'mongoose';
+import { RelationTypeEnumT } from '@enums/index';
 
 const RelationSchema = new Schema({
   source: { type: Schema.Types.ObjectId, ref: 'Person', required: true },
   target: { type: Schema.Types.ObjectId, ref: 'Person', required: true },
-  type: { type: Number, required: true },
+  type: { type: String, enum: Object.values(RelationTypeEnumT) , required: true },
 }, { timestamps: true });
 
 const Relation = mongoose.model('Relation', RelationSchema, 'relations');

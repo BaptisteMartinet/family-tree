c17faf8bd3c4b3a0d38234de81dcc3340b1734c6
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-01-08 01:42:20 +0100

    fix: change RelationArgs to use new targetId field

--- src/input-types/index.ts
+++ src/input-types/index.ts
@@ -2,14 +2,14 @@ import { GraphQLInputObjectType, GraphQLNonNull, GraphQLString } from 'graphql';
 import { RelationTypeEnum, RelationTypeEnumT } from '@enums/index';
 
 export interface RelationArgsT {
-  target: string,
+  targetId: string,
   type: RelationTypeEnumT,
 }
 
 export const RelationArgs = new GraphQLInputObjectType({
   name: 'RelationArgs',
   fields: {
-    target: { type: new GraphQLNonNull(GraphQLString) },
+    targetId: { type: new GraphQLNonNull(GraphQLString) },
     type: { type: new GraphQLNonNull(RelationTypeEnum) },
   },
 });
